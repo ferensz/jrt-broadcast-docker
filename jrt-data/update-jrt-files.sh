1d8bea691cae4ec2bf03ba92677e2f6a7b84e2f0
Ferenc Balogh <ferenc.balogh@techieve.hu>
2024-01-19 08:21:13 +0100

    Updated the git repo inside update-jrt-files.sh

--- jrt-data/update-jrt-files.sh
+++ jrt-data/update-jrt-files.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
 cd /tmp
-git clone https://github.com/mpapenbr/jrt-data.git
+git clone https://github.com/ferensz/jrt-data.git
 rm -rf /data
 unzip -d /data jrt-data/jrt-data.zip
 rm -rf /tmp/jrt-data
-
